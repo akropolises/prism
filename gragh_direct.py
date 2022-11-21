a02849f390bb26482dc1b2d4780ae977287e24ff
akropolises <onk.10781@gmail.com>
2022-11-21 21:04:25 +0900

    最新の状態

--- gragh_direct.py
+++ gragh_direct.py
@@ -73,6 +73,7 @@ def LPver(mode:str, deg = 45, flatisout = True, LP = 40, recommend = False):
                 thetaASKAout, phiASKAout = ASKAdict[mode]
                 theta_out0 = thetaASKAout - deg
                 if not (-90 < theta_out0 < 90):
+                    continue
                     if mode != "I":
                         theta.append(thetaASKAout)
                         phi.append(phiASKAout)
@@ -111,6 +112,7 @@ def LPVver(mode:str, deg = 45, flatisout = True, LPV = 90, recommend = False):
                 thetaASKAout, phiASKAout = ASKAdict[mode]
                 theta_out0 = thetaASKAout - deg
                 if not (-90 < theta_out0 < 90):
+                    continue
                     if mode != "I":
                         theta.append(thetaASKAout)
                         phi.append(phiASKAout)
@@ -147,6 +149,7 @@ def prismver(mode:str, recommend = False):
             thetaASKAout, phiASKAout = ASKAdict[mode]
             theta_out = prism(thetaASKAout,siten=True)
             if not (-90 < theta_out < 90):
+                continue
                 if mode != "I":
                     theta.append(thetaASKAout)
                     phi.append(phiASKAout)
@@ -184,6 +187,7 @@ def idealver(mode:str, deg = 45, recommend = False):
             thetaASKAout, phiASKAout = ASKAdict[mode]
             theta_out0 = thetaASKAout - deg
             if not (-90 < theta_out0 < 90):
+                continue
                 if mode != "I":
                     theta.append(thetaASKAout)
                     phi.append(phiASKAout)
@@ -291,15 +295,15 @@ def All_mode(mode:str, deg = 45, flatisout = True, LP = 40, LPV = 90, recommend 
 def All_condition(mode:str, deg = 45, flatisout = True, LP = 40, LPV = 90, recommend = False):
     assert(mode in {"I","W","X","Y"})
     x,y = non(mode, deg=deg, recommend=recommend)
-    plt.scatter(x, y, label="no refraction", s=5)
+    plt.scatter(x, y, label="屈折なし", s=5)
     x,y = LPver(mode, deg=deg, flatisout=flatisout, LP=LP, recommend=recommend)
     plt.scatter(x, y, label="LP%d"%LP, s=5)
     x,y = LPVver(mode, deg=deg, flatisout=flatisout, LPV=LPV, recommend=recommend)
     plt.scatter(x, y, label="LPV%d"%LPV, s=5)
     x,y = prismver(mode, recommend=recommend)
-    plt.scatter(x, y, label="prism", s=5)
+    plt.scatter(x, y, label="直角プリズム", s=5)
     x,y = idealver(mode, deg=deg, recommend=recommend)
-    plt.scatter(x, y, label="ideal", s=5)
+    plt.scatter(x, y, label="理想状態", s=5)
     
     titledict = {"I":"結像光", "W":"両側透過光", "X":"X側透過光", "Y":"Y側透過光"}
     plt.title(titledict[mode], fontname="MS Gothic")
@@ -313,7 +317,8 @@ def All_condition(mode:str, deg = 45, flatisout = True, LP = 40, LPV = 90, recom
 def compare_deg(mode:str, degs = [45], flatisout = True, LP = 40, LPV = 90, recommend = False):
     assert(mode in {"non", "LP", "LPV", "prism", "ideal"})
     labeldict = {"W": "両側透過光", "X":"X側透過光", "Y":"Y側透過光", "I":"結像光"}
-    Mdeg = max(deg)
+    Mdeg = max(degs)
+    mdeg = min(degs)
     for deg in degs:
         for i in ("I","W","X","Y"):
             if mode == "non":
@@ -337,24 +342,17 @@ def compare_deg(mode:str, degs = [45], flatisout = True, LP = 40, LPV = 90, reco
             plt.scatter(nx, y, label=labeldict[i], s=5)
         plt.xlabel("方位角 [deg]", fontname="MS Gothic")
         plt.ylabel("仰角 [deg]", fontname="MS Gothic")
-        plt.xlim(-90,90+Mdeg)
+        plt.xlim(-90+mdeg,90+Mdeg)
         plt.ylim(-90,90)
         plt.legend(prop={"family":"MS Gothic"})
         plt.show()
 
 if __name__ == "__main__":
     # All_mode("All",recommend=True)
-    # All_mode("ideal",deg=40)
     # for i in ("I","W","X","Y"):
     #     unitPlot(i,"non")
     # for i in (30,40):
-    #     for j in (30,45,50):
-    #         All_mode("LP",deg=j, LP=i, flatisout=False, recommend=True)
-    # for i in (30,45,50):
-        # All_mode("LPV",deg = i, LPV=90)
-        # All_mode("LP",deg = i, LP=30, flatisout=False, recommend=True)
-    # All_mode("LP",deg = 50, LP=40, recommend=True)
-    # All_mode("LP",deg = 60, LP=60)
-    # All_mode("prism")
+    #     compare_deg("LP",degs=[30,45,50],LP=i)
+    # compare_deg("LPV",degs=[45,70],LPV=130)
     # All_condition("W")
     pass
